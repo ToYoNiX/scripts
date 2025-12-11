a58f5daa84043593f8935966436fdf46de10a461
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-12-11 15:30:32 +0200

    adding if the battery exist condition.

--- status.sh
+++ status.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 #
 
+BAT="/sys/class/power_supply/BAT0"
+
 while true
 do
-    echo BAT: $(cat /sys/class/power_supply/BAT0/capacity)% '|' $(date)
+    output=""
+    output=$output\ $(date)
+
+    if [ -d $BAT ]
+    then
+        PERCENTAGE=BAT:\ $(cat $BAT/capacity)%
+        output=$PERCENTAGE\ \|\ $output
+    fi
+
+    echo $output
     sleep 5
 done
