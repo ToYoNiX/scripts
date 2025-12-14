77bc23e1d9abecb24157ea917f258389bdd5ee91
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-12-14 22:30:26 +0200

    updating the wallpaper.

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-swaybg -i ~/Pictures/Wallpapers/wallhaven-ly9qzq.jpg &
+swaybg -i ~/Pictures/Wallpapers/wallhaven-7j3lve.png &
 flatpak run com.discordapp.Discord --start-minimized &
 nextcloud &
 lxpolkit &
