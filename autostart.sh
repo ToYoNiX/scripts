cddb1120e6e51cf58e12dba1f1651741250d6a8f
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-12-22 16:17:36 +0200

    adding syncthing and syncthing tray.

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,8 @@
 
 swaybg -i  $(cat ~/.cache/wal/wal) &
 flatpak run com.discordapp.Discord --start-minimized &
+syncthing serve --no-browser --no-restart &
+syncthingtray & 
 nextcloud &
 swhks &
 swhkd
