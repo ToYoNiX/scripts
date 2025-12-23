56d4412f1522a6eee8f8c7a92efa1fa6fc67f25c
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-12-23 17:22:05 +0200

    removing nextcloud.

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,5 @@ swaybg -i  $(cat ~/.cache/wal/wal) &
 flatpak run com.discordapp.Discord --start-minimized &
 syncthing serve --no-browser --no-restart &
 syncthingtray & 
-nextcloud &
 swhks &
 swhkd
