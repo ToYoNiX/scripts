c2ff28b5f69c67823ba75a0e2b322972018d9e7a
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-12-28 00:32:16 +0200

    adding mode fill into swaybg command.

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-swaybg -i  $(cat ~/.cache/wal/wal) &
+swaybg -i  $(cat ~/.cache/wal/wal) -m fill &
 flatpak run com.discordapp.Discord --start-minimized &
 syncthing serve --no-browser --no-restart &
 syncthingtray & 
